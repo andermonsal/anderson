3baca295f65e382af5f1a98afc383d87517dcb1c
USER_NAME <USER_EMAIL>
2025-03-25 15:28:59 -0500

    Commit inicial - subir app

---
+++ suma.py
@@ -0,0 +1,10 @@
+def calcular_sumas():
+    numero1 = 15
+    numero2 = 27
+    suma1 = numero1 + numero2
+    print(f"Suma de {numero1} + {numero2} = {suma1}")
+    lista = [10, 20, 30, 40]
+    suma2 = sum(lista)
+    print(f"Suma de lista {lista} = {suma2}")
+
+calcular_sumas()
